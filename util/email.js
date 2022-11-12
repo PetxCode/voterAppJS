7dfcc1b90c27689bece909347b85a757f8c99631
unknown <brighterdayscodelab@gmail.com>
2022-11-12 23:13:22 +0100

    save

--- util/email.js
+++ util/email.js
@@ -53,52 +53,7 @@ const verifiedUser = async (getUser) => {
       from: "AJ Vote ❤❤❤ <newstudentsportal2@gmail.com>",
       to: getUser?.email,
       subject: "Account Verification",
-      html: `  <container>
-    <row>
-      <columns small="12" >
-        
-        <table class="one-column" border="0" cellpadding="0" cellspacing="0" width="100%" style="border-spacing:0; border-left:1px solid #e8e7e5; border-right:1px solid #e8e7e5; border-bottom:1px solid #e8e7e5; padding: 10px" bg="#FFFFFF">
-          
-          <div width="350" height="150" align="center" style="border-radius: 50%;">
-           <img src="https://res.cloudinary.com/dv4dlmp4e/image/upload/v1667857306/NYCNi_wvwlde.png" width="60" height="60" style="padding-top:5px" alt="" border="0"/>
-            <img src="https://res.cloudinary.com/dv4dlmp4e/image/upload/v1667857308/Lagos_kidbfs.png" width="60" height="60" style="padding-top:5px" alt="" border="0"/>
-          </div>
-
-          <tr>
-            <td align="center" style="padding:0px 40px 40px 40px"><p style="color:#262626; font-size:32px; text-align:center; font-family: Verdana, Geneva, sans-serif">Hello! <br> ${getUser.fullName}</p>
-                <b style="color:#000000; font-size:16px; text-align:left; font-family: Verdana, Geneva, sans-serif; line-height:22px ">
-                Congratulations!!!</b><br/>
-                Thank you for creating an account with "Ajegunle Youth Council Voting Platform", <br/>Now you can carry out your VOTING RIGHT successfully <br/>
-                
-                <strong style="color: red">Please follow the instruction to finish up!</strong>  </br>
-                <br/>
-                <br/>
-                Use this Button below to get your voting code access from your organisation <strong > ${getUser.orgName} </strong>!admin!!!
-
-                <div>
-                  <br/>
-                  <br/>
-                  <a 
-                    href="http://localhost:3000/api/user/${getUser._id}/token" 
-                  referrerpolicy="no-referrer" target="_blank" 
-                  style="text-decoration: none;
-                  padding: 20px 30px;
-                  color: white;
-                  margin-top: 50px;
-                  background-color: #000269;
-                  border-radius: 2px;
-                  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
-                  "
-                  >Request for your Code</a>
-                </div>
-          
-                <br />
-            </p></td>
-          </tr>
-        </table>
-      </columns>
-    </row>
-</container>`,
+      html: data,
     };
 
     myTransport.sendMail(mailOptions, () => {
@@ -316,3 +271,50 @@ module.exports = {
   resetMyPassword,
   acceptance,
 };
+
+// `  <container>
+//     <row>
+//       <columns small="12" >
+
+//         <table class="one-column" border="0" cellpadding="0" cellspacing="0" width="100%" style="border-spacing:0; border-left:1px solid #e8e7e5; border-right:1px solid #e8e7e5; border-bottom:1px solid #e8e7e5; padding: 10px" bg="#FFFFFF">
+
+//           <div width="350" height="150" align="center" style="border-radius: 50%;">
+//            <img src="https://res.cloudinary.com/dv4dlmp4e/image/upload/v1667857306/NYCNi_wvwlde.png" width="60" height="60" style="padding-top:5px" alt="" border="0"/>
+//             <img src="https://res.cloudinary.com/dv4dlmp4e/image/upload/v1667857308/Lagos_kidbfs.png" width="60" height="60" style="padding-top:5px" alt="" border="0"/>
+//           </div>
+
+//           <tr>
+//             <td align="center" style="padding:0px 40px 40px 40px"><p style="color:#262626; font-size:32px; text-align:center; font-family: Verdana, Geneva, sans-serif">Hello! <br> ${getUser.fullName}</p>
+//                 <b style="color:#000000; font-size:16px; text-align:left; font-family: Verdana, Geneva, sans-serif; line-height:22px ">
+//                 Congratulations!!!</b><br/>
+//                 Thank you for creating an account with "Ajegunle Youth Council Voting Platform", <br/>Now you can carry out your VOTING RIGHT successfully <br/>
+
+//                 <strong style="color: red">Please follow the instruction to finish up!</strong>  </br>
+//                 <br/>
+//                 <br/>
+//                 Use this Button below to get your voting code access from your organisation <strong > ${getUser.orgName} </strong>!admin!!!
+
+//                 <div>
+//                   <br/>
+//                   <br/>
+//                   <a
+//                     href="http://localhost:3000/api/user/${getUser._id}/token"
+//                   referrerpolicy="no-referrer" target="_blank"
+//                   style="text-decoration: none;
+//                   padding: 20px 30px;
+//                   color: white;
+//                   margin-top: 50px;
+//                   background-color: #000269;
+//                   border-radius: 2px;
+//                   box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+//                   "
+//                   >Request for your Code</a>
+//                 </div>
+
+//                 <br />
+//             </p></td>
+//           </tr>
+//         </table>
+//       </columns>
+//     </row>
+// </container>`;
